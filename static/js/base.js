2b1b2464eb569e4d34740e55e2d4635eef75c3cc
cula <culahuang@gmail.com>
2016-01-19 22:46:17 +0800

    Add macro

--- static/js/base.js
+++ static/js/base.js
@@ -43,6 +43,20 @@ function formInit(form) {
     return false;
 }
 
+function checkPasswordSame() {
+  var newPassword = $('input#newPassword').val();
+  var newPasswordAgain = $('input#newPasswordAgain').val();
+  if (newPassword == newPasswordAgain) {
+    $('#newPasswordGroup').removeClass('has-error');
+    $('#newPasswordError').text('');
+    $('#newPasswordSubmit').removeAttr('disabled');
+  } else {
+    $('#newPasswordGroup').addClass('has-error');
+    $('#newPasswordError').text('两次输入密码不一致.');
+    $('#newPasswordSubmit').attr('disabled', 'disabled');
+  }
+}
+
 $(function () {
   $('.form').each(function() {
     formInit($(this));
@@ -56,4 +70,9 @@ $(function () {
     todayHighlight: 'true',
     autoclose: true
   });
+
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('.row-offcanvas').toggleClass('active')
+  });
+
 });
